874743881e758830a8d7f18554161e964271a6c2
arthur <arthur.timoteo@gmail.com>
2025-09-07 18:40:57 -0300

    feat: [PRODUCER] Project created

---
+++ producer_application/src/test/java/github/arthur/lswqd/LswqdApplicationTests.java
@@ -0,0 +1,13 @@
+package github.arthur.lswqd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LswqdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
