1fd67c44e060eed5cb05355319dabff1a0cbb591
arthur <arthur.timoteo@gmail.com>
2025-09-13 21:24:41 -0300

    feat: [PRODUCER] Add connection with Kafka

--- producer_application/src/main/java/github/arthur/lswqd/LswqdApplication.java
+++ producer_application/src/main/java/github/arthur/lswqd/LswqdApplication.java
@@ -1,13 +1,21 @@
 package github.arthur.lswqd;
 
+import github.arthur.lswqd.infra.kafka.LogProducerService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class LswqdApplication {
+	private static LogProducerService _logProducerService;
+
+	public LswqdApplication(LogProducerService producerService) {
+		this._logProducerService = producerService;
+	}
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(LswqdApplication.class, args);
+		_logProducerService.sendMessage("opa");
 	}
 
 }
