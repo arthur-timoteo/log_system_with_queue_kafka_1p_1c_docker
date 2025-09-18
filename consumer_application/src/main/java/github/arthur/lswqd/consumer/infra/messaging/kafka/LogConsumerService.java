d9f7931b849136c433ffb167deb58cae4cbff05d
arthur <arthur.timoteo@gmail.com>
2025-09-17 22:22:34 -0300

    feat: [CONSUMER] Add connection with kafka

---
+++ consumer_application/src/main/java/github/arthur/lswqd/consumer/infra/messaging/kafka/LogConsumerService.java
@@ -0,0 +1,13 @@
+package github.arthur.lswqd.consumer.infra.messaging.kafka;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LogConsumerService {
+
+    @KafkaListener(topics = "${app.kafka.topic}", groupId = "${spring.kafka.listener.group-id}")
+    public void consume(String log) {
+        System.out.println("📥 log received: " + log);
+    }
+}
