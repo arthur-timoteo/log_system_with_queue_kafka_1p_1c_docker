c4593ca6d8073aaa20e46b7fcd572eb97e8a0e4a
arthur <arthur.timoteo@gmail.com>
2025-09-27 23:52:21 -0300

    feat: [CONSUMER] Implement AVRO to Kafka

--- consumer_application/src/main/java/github/arthur/lswqd/consumer/infra/messaging/kafka/LogConsumerService.java
+++ consumer_application/src/main/java/github/arthur/lswqd/consumer/infra/messaging/kafka/LogConsumerService.java
@@ -1,5 +1,6 @@
 package github.arthur.lswqd.consumer.infra.messaging.kafka;
 
+import com.resources.avro.SchemaGeneralLog;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -7,7 +8,7 @@ import org.springframework.stereotype.Service;
 public class LogConsumerService {
 
     @KafkaListener(topics = "${app.kafka.topic}", groupId = "${spring.kafka.listener.group-id}")
-    public void consume(String log) {
+    public void consume(SchemaGeneralLog log) {
         System.out.println("📥 log received: " + log);
     }
 }
